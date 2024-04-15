6e81b5ae02d199f8d84a100b4786b7411e871c45
Daniel Dieguez <dieguezd91@gmail.com>
2024-04-15 19:35:10 -0300

    cooldown + enemy attack + Lose round

--- Flag Fights/Assets/Scripts/FSM/EnemyStates/EnemyStateAttack.cs
+++ Flag Fights/Assets/Scripts/FSM/EnemyStates/EnemyStateAttack.cs
@@ -8,6 +8,8 @@ public class EnemyStateAttack<T> : State<T>
     LineOfSight _LOS;
     T _chaseInput;
     float _attackRange;
+    float _cooldownTime = 3.0f;
+    float _lastAttackTime;
 
     public EnemyStateAttack(Animator animator, LineOfSight lOS, T chaseInput, float attackRange)
     {
@@ -16,22 +18,35 @@ public class EnemyStateAttack<T> : State<T>
         _chaseInput = chaseInput;
         this._attackRange = attackRange;
     }
+
     public override void Enter()
     {
-        _animator.SetBool("Attacking", true);
+        
     }
+
     public override void Execute()
     {
         base.Execute();
-        
-        Debug.Log("Enemy attack");
+
+        if (Time.time - _lastAttackTime >= _cooldownTime)
+        {
+            _animator.SetTrigger("Attack");
+            _lastAttackTime = Time.time;
+            DealDamage();
+        }
 
         if (!_LOS.HasLOS(_attackRange))
             _fsm.Transition(_chaseInput);
-
     }
+
+    void DealDamage()
+    {
+        Debug.Log("Dañado");
+        GameManager.instance.LoseRound();
+    }
+
     public override void Sleep()
     {
-        _animator.SetBool("Attacking", false);
+        
     }
 }
